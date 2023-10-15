871251f7cf3a7b69c7ba2f356b859233036baa3f
git-sunao <sunaosugiyama@gmail.com>
2023-10-16 03:55:30 +0900

    updated link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,10 @@
+unlink ${HOME}/.bashrc
 ln -s ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
 unlink ${HOME}/.config
 ln -s ${HOME}/dotfiles/.config ${HOME}/.config
+unlink ${HOME}/.latexmkrc
 ln -s ${HOME}/dotfiles/.latexmkrc ${HOME}/.latexmkrc
+unlink ${HOME}/.gitignore_global
 ln -s ${HOME}/dotfiles/.gitignore_global ${HOME}/.gitignore_global
+unlink ${HOME}/.inputrc
 ln -s ${HOME}/dotfiles/.inputrc ${HOME}/.inputrc
